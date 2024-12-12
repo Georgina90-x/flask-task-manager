a7207d1318da97d73b70ba1647ea5dfd8652e117
Georgina Kidger <georgina.kidger@hotmail.com>
2024-12-12 21:19:48 +0000

    created files and taskmanager database

---
+++ run.py
@@ -0,0 +1,11 @@
+import os
+from taskmanager import app
+from taskmanager import db
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG")
+    )
